77caff9aed20b1ea466c6195f57932b4c5e6a4ff
Bogdan Zuy <bogdan@zuy.name>
2015-11-29 00:14:33 +0300

    Create dialog.php

---
+++ dialog.php
@@ -0,0 +1,7 @@
+<iframe width="100%" height="500px" id="docs"></iframe>
+
+<button onclick="codiad.modal.unload();return false;">Close</button>
+
+<script>
+    $('#docs').attr('src','http://sass-lang.com/guide');
+</script>
