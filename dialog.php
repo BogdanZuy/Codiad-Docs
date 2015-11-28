c3bef073da52aa97f7801bdf2ff36a6d7176ce2d
Bogdan Zuy <bogdan@zuy.name>
2015-11-29 00:28:40 +0300

    Update dialog.php

--- dialog.php
+++ dialog.php
@@ -1,7 +1,14 @@
+<select id="docs-select">
+    <option value="https://www.google.com/search?q=http://docs.emmet.io&btnI=Im+Feeling+Lucky">Emmet</option>
+    <option value="https://www.google.com/search?q=http://sass-lang.com/guide&btnI=Im+Feeling+Lucky">Sass</option>
+</select>
 <iframe width="100%" height="500px" id="docs"></iframe>
 
 <button onclick="codiad.modal.unload();return false;">Close</button>
 
 <script>
+    $('#docs-select').change(function(){
+        $('#docs').attr('src',$(this).val());    
+    });
     $('#docs').attr('src','https://sass-lang.com/guide');
 </script>
